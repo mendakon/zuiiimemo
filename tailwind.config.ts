d1a579066a2d9ca8e73bdf8835ed23ad2891822b
kuchibashi <kuchibashimendakon@gmail.com>
2025-05-27 00:22:25 +0900

    tailwind.config.tsのダークモード設定を修正し、プラグインを空に変更。theme-provider.tsxでのインポート文を簡略化。

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,7 @@
 import type { Config } from "tailwindcss"
 
 const config = {
-  darkMode: ["class"],
+  darkMode: "class",
   content: [
     "./pages/**/*.{ts,tsx}",
     "./components/**/*.{ts,tsx}",
@@ -75,7 +75,7 @@ const config = {
       },
     },
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [],
 } satisfies Config
 
 export default config
